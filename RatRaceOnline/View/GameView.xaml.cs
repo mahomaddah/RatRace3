e3e6c90adce575aeb6ea011be668e1bef4d50f15
Maho <37220035+mahomaddah@users.noreply.github.com>
2024-12-05 20:33:15 +0300

    navigation mobile ifx Before card Index Binding

--- RatRaceOnline/View/GameView.xaml.cs
+++ RatRaceOnline/View/GameView.xaml.cs
@@ -1,9 +1,21 @@
 namespace RatRace3.View;
+using RatRace3.View;
+using Syncfusion.Maui.Cards;
 
 public partial class GameView : ContentPage
 {
 	public GameView()
 	{
 		InitializeComponent();
-	}
+        
+    }
+
+    private async void paymentPageNavBtn_Clicked(object sender, EventArgs e)
+    {
+		if(sender!=null)
+		{
+            await DisplayAlert("Alert", "Payments clicked...(GameView class)", "OK");
+        }
+    }
 }
+
