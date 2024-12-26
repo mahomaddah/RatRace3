e6ac1671fc59c25191830793a51c14c0a95575a7
Maho <37220035+mahomaddah@users.noreply.github.com>
2024-12-26 20:16:25 +0300

    Click on Company Created ...

--- RatRaceOnline/View/GameView.xaml.cs
+++ RatRaceOnline/View/GameView.xaml.cs
@@ -1,11 +1,12 @@
 namespace RatRace3.View;
 using RatRace3.View;
-
-
+using RatRace3.ViewModel;
+using Syncfusion.Maui.Rotator;
 
 public partial class GameView : ContentPage
 {
     public CardViewModel CardviewModel { get; set; }
+    public RotatorViewModel IpoCompaniesVM { get; set; }
     public GameView()
 	{
 		InitializeComponent();
@@ -21,8 +22,10 @@ public partial class GameView : ContentPage
         DeviceDisplay.MainDisplayInfoChanged += (s, e) => UpdateOrientation();
 
         //binding VM to Listview ....
-     //   LVcompaiesMarket.ItemsSource = CardviewModel.StockMarketCompanys;
+        //   LVcompaiesMarket.ItemsSource = CardviewModel.StockMarketCompanys;
+        IpoCompaniesVM = new RotatorViewModel();
 
+        CompanyInvestRoter.ItemsSource = IpoCompaniesVM.RotatorItems;
 
 
     }
@@ -69,6 +72,18 @@ public partial class GameView : ContentPage
     {
         await DisplayAlert("Income...", "$1,200.00 Total Income Collected!","Amazing! $$$");
     }
-  
+
+    private async void CompanyInvestRoter_ItemTapped(object sender, EventArgs e)
+    {
+        SfRotator router = new SfRotator();
+        router = (SfRotator)sender;
+
+        SfRotatorItem selectedCompany = IpoCompaniesVM.RotatorItems[router.SelectedIndex];
+        if(selectedCompany!= null)
+        {
+            await  DisplayAlert("Selected company's image, symbol and price ... Send them to Market page by Shell goToAsync parameter ... ",selectedCompany.Image,"OK");
+        }
+        await Shell.Current.GoToAsync("MarketPage");
+    }
 }
 
